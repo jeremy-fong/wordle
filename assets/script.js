667cabf4e3caff7f8561b150d5a2fb0098997d05
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-04 14:37:46 -0800

    fixed to mark which guesses are in correct position and wrong position

--- assets/script.js
+++ assets/script.js
@@ -60,6 +60,18 @@ function initialize() {
 
 function update() {
     let correct = 0;
+    let letterCount = {};
+    for (let i = 0; i < word.length; i++) {
+        letter = word[i];
+        if (letterCount[letter]) {
+            letterCount[letter] += 1;
+        }
+        else {
+            letterCount[letter] = 1;
+        }
+    }
+
+    // first check, check all the correct ones
     for (let c = 0; c < width; c++) {
         let currTile = document.getElementById(row.toString() + "-" + c.toString());
         let letter = currTile.innerText;
@@ -68,16 +80,28 @@ function update() {
         if (word[c] == letter) {
             currTile.classList.add("correct");
             correct += 1;
-        } // Is it in the word?
-        else if (word.includes(letter)) {
-            currTile.classList.add("present");
-        } // Not in word
-        else {
-            currTile.classList.add("absent");
-        }
+            letterCount[letter] -= 1;
+        } 
         
         if (correct == width) {
             gameOver = true;
+        }
+    }
+
+    // run again and mark which ones are present but in wrong position 
+    for (let c = 0; c < width; c++) {
+        let currTile = document.getElementById(row.toString() + "-" + c.toString());
+        let letter = currTile.innerText;
+
+        if (!currTile.classList.contains("correct")) {
+            // Is it in the word?
+             if (word.includes(letter) && letterCount[letter] > 0) {
+                 currTile.classList.add("present");
+                 letterCount[letter] -= 1;
+             } // Not in word
+             else {
+                 currTile.classList.add("absent");
+             }
         }
     }
 }
