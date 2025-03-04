4493d4c887ed3f4f1cbc88247b4f6622aeefd828
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2025-03-03 23:50:26 -0800

    added code to guess letters and delete letters for the word

--- assets/script.js
+++ assets/script.js
@@ -22,4 +22,39 @@ function initialize() {
             document.getElementById("board").appendChild(tile);
         }
     }
+
+    // key press for guess
+    document.addEventListener("keyup", (e) => {
+        if (gameOver) return;
+
+        // alert(e.code);
+        if("KeyA" <= e.code && e.code <= "KeyZ") {
+            if (col < width) {
+                let currTile = document.getElementById(row.toString() + "-" + col.toString());
+                if (currTile.innerText == "") {
+                    currTile.innerText =  e.code[3];
+                    col += 1;
+                }
+            }
+        }
+        else if (e.code == "Backspace") {
+            if (0 < col && col <= width) {
+                col -=1;
+                let currTile = document.getElementById(row.toString() + "-" + col.toString());
+                currTile.innerText ="";
+            }
+        }
+        else if (e.code == "Enter") {
+            update();
+            row += 1; //start new row
+            col = 0; //start at 0 for new row
+
+        }
+
+        if (!gameOver && row == height) {
+            gameOver = true;
+            document.getElementById("answer").innerText = word;
+        }
+    })
 }
+
